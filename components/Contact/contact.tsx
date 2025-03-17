c854cf2583e42ca1e1f032bdd919550687b6f0dd
LachlanRowley-AA <lachlan@assetalley.com.au>
2025-03-17 15:25:03 +1100

    Added missing type

--- components/Contact/contact.tsx
+++ components/Contact/contact.tsx
@@ -45,7 +45,7 @@ export function AuthenticationForm({
     form.setFieldValue('ref', params.get('ref'));
   }, []);
 
-  const handleCheckboxChange = (value) => {
+  const handleCheckboxChange = (value : any) => {
     setSelectedCheckboxes(value);
   };
 
